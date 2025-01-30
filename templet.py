148e458ec2ae7f7514eace24b2bc14595a47d557
naskar002 <rupapriyanaskar2002@gmail.com>
2025-01-30 20:55:33 +0530

    templet updated

---
+++ templet.py
@@ -0,0 +1,54 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format = '[%(asctime)s]: %(message)s:')
+
+project_name = 'Hate_Speech'
+
+file_list = [
+    f"{project_name}/components/__init__.py", #local package i.e. need to include consturctor
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py", #as i need to push the file to the gcp cloud
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/configuration/gcloud_syncer.py",
+    f"{project_name}/contant/__init__.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
+    f"{project_name}/pipeline/train_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    f"{project_name}/ml/__init__.py",
+    f"{project_name}/ml/feature.py",
+    f"{project_name}/ml/models.py",
+    "app.py",
+    "demo.py",
+    "requirements.txt"
+    "Dockerfile",
+    "setup.py",
+    ".dockerignore"  
+]
+
+for file_path in file_list:
+    file_path = Path(file_path)
+
+    file_dir,filename = os.path.split(file_path)
+
+    if file_dir != "":
+        os.makedirs(file_dir,exist_ok=True)
+        logging.info(f"Creating directory: {file_dir} for the file: {filename}")
+
+    if(not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path,"w") as f:
+            pass
+            logging.info(f"Creating empty file: {file_path}")
+
+    else:
+        logging.info(f"{filename} is already exists")
